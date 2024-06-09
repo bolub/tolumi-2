4c600898fb574692d51bd755687b78265bbfa9dd
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-06-09 13:22:15 +0300

    Cleanup

---
+++ src/fonts.ts
@@ -0,0 +1,11 @@
+import { Manrope, Playfair_Display } from "next/font/google";
+
+export const inter = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const roboto_mono = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+});
